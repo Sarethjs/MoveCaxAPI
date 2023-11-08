fa4a111c8192a5bc650cb3e0cbe86223d19e1db6
Sareth <himura.js@hotmail.com>
2023-11-08 00:51:21 -0500

    Sarlocation model added

---
+++ model/sarlocation.js
@@ -0,0 +1,24 @@
+const haversine = require('haversine');
+
+class SarLocation {
+    constructor (lat, lon, routeIdentifier) {
+        this.lat = lat;
+        this.lon = lon;
+        this.routeIdentifier = routeIdentifier;
+    }
+
+    distanceTo(location) { 
+
+        const start = {
+            latitude: this.lat,
+            longitude: this.lon
+        };
+
+        const end = {
+            latitude: location.lat,
+            longitude: location.lon
+        };
+
+        return haversine(start, end, {unit: 'meters'});
+    }
+}
